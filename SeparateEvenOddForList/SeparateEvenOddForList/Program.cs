84c16267e1f26501acb655370d1fc4dc0fb842d1
Ali Raza <zaqetyx@gmail.com>
2023-05-07 16:38:51 +0500

    basic c program

---
+++ SeparateEvenOddForList/SeparateEvenOddForList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateEvenOddForList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] ar = new string[10];
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = Console.ReadLine();
+                Console.WriteLine( ar);
+                Console.ReadLine();
+
+
+            }
+
+
+        }
+    }
+}
